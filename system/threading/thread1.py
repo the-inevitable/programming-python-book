874447d69cbf1470e911ecfd3979ba55d7130b60
Vladyslav <vkhymenkov@gmail.com>
2020-04-12 18:39:29 +0300

    _thread

---
+++ system/threading/thread1.py
@@ -0,0 +1,21 @@
+"""
+Spawn threads untill "q" is entered at input.
+"""
+
+import _thread
+
+
+def child(tid):
+    print(f'Hello from thread {tid}')
+
+
+def parent():
+    i = 0
+    while True:
+        i += 1
+        _thread.start_new_thread(child, (i,))
+        if input() == 'q':
+            break
+
+
+parent()

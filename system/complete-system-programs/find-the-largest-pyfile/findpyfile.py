7be4728ba62760a193995b2b0bb0d4d9f8f8c48b
Vladyslav <vkhymenkov@gmail.com>
2020-04-25 23:14:48 +0300

    dana loves vlad

---
+++ system/complete-system-programs/find-the-largest-pyfile/findpyfile.py
@@ -0,0 +1,21 @@
+"""
+Find the largest file with a given extension in a given directory.
+"""
+
+import os
+import sys
+import glob
+import argparse
+
+
+def find_largest_file(path, ext):
+    all_files = glob.glob(os.sep.join([path, ext]))
+    all_sizes = [os.path.getsize(filename) for filename in all_files]
+    return max(all_sizes)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Find the largest file.')
+
+    parser.add_argument('--path', required=True)
+    parser.add_argument('--ext', required=True)love vladika from danochka

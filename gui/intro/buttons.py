14c130f50d0e586b47aa8d41d1ff9e88f5b68906
Vlad Khymenko <vkhymenkov@gmail.com>
2020-05-25 23:34:46 +0300

    pass arguments to a handler

--- gui/intro/buttons.py
+++ gui/intro/buttons.py
@@ -3,11 +3,11 @@ import sys
 from tkinter import *
 
 
-def quit_handler():
-    print('Exiting...')
+def quit_handler(some_arg):
+    print(f'Exiting... {some_arg}')
     sys.exit()
 
 
-widget = Button(None, text='Just a button', command=quit_handler)
+widget = Button(None, text='Just a button', command=lambda: quit_handler('some value'))
 widget.pack(expand=YES, fill=BOTH)
 widget.mainloop()
